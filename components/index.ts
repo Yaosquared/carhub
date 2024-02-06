cf3c68c373c667d3bd8e5f1a0a4649783b180f72
Yaosquared <marioyaoyao14@gmail.com>
2024-02-06 14:53:34 +0800

    added car catalogue

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,21 @@
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 import Footer from "./Footer";
 
-export { Hero, CustomButton, Navbar, Footer };
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  Footer,
+};

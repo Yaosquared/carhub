be3ca2fac20785a84a3ada6a20ed1d1c21e571fb
Yaosquared <marioyaoyao14@gmail.com>
2024-02-07 15:04:09 +0800

    added additional styles and types

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.png" />
+      </head>
       <body className="relative">
         <Navbar />
         {children}
